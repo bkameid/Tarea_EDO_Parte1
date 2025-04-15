484a52d8a614a0b8471d51f18a9d1bf4b5ea26c6
Daniel Valenzuela Martínez <daniel.valenzuela.m@ug.uchile.cl>
2025-04-14 23:43:21 -0400

    Update greeting message in main function of Tarea_EDO.py

--- Tarea_EDO.py
+++ Tarea_EDO.py
@@ -3,5 +3,4 @@
 # Cambio en la 2a Rama
 
 def main():
-    # Esta función es el punto de entrada del programa
-    print("Hola, mundo!")  # Imprime un saludo en la consola
+    print("Aguante Beauchef furs")
